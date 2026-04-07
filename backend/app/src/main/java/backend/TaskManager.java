b35b1eb6b44367a3b8a4c2bca7d552dd47d7a6a1
Austin Zanoni <austinzanoni21@gmail.com>
2026-04-07 08:56:30 -0600

    Initial create for java backend

---
+++ backend/app/src/main/java/backend/TaskManager.java
@@ -0,0 +1,12 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManager.class, args);
+    }
+}
